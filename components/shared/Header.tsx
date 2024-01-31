11fa8fa3143dc573d447ed1aed9c16748027846b
Prabuddha Chatterjee <prabuddhapiku@gmail.com>
2024-01-31 23:11:37 +0530

    Header,Footer and Services Carousel added

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -38,15 +38,12 @@ const Header = () => {
     }, [controlNavbar]);
     return (
         <header
-
-            className={`mx-5 mt-2 border-b text-xl rounded-full ps-5 pe-3 py-2 flex bg-white top-0 sticky z-10 transition-transform duration-300 transform ${show ? 'translate-y-0' : '-translate-y-full'}`}
-        >
+            className={`mx-5 mt-2 border-b text-xl rounded-full ps-5 pe-3 py-2 flex bg-white top-0 sticky z-10 transition-transform duration-300 transform ${show ? 'translate-y-0' : '-translate-y-full'}`}>
             <div className='w-full flex'>
                 <div className="flex items-center justify-between w-full">
-                    <div className='w-[100%] flex items-center justify-center md:w-[300px]'>
-
-                        <Link href='/' className="  px-3 ">
-                            <Image src='/icons/faslane_logo.png' alt="Faslane Logo" width={280} height={200} objectFit='cover' />
+                    <div className='w-[100%] flex items-center justify-start md:w-[300px]'>
+                        <Link href='/'>
+                            <Image src='/icons/faslane_logo.png' alt="Faslane Logo" width={180} height={200} objectFit='cover' />
                         </Link>
                     </div>
                     <nav className="md:flex hidden w-full text-white">
